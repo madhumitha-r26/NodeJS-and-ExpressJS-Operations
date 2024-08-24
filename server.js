b126a1a45db00c0a54974b72bf532b9c32f32df7
Madhumitha R <98893549+madhumitha-r26@users.noreply.github.com>
2024-08-24 10:37:29 +0000

    server creation

---
+++ server.js
@@ -0,0 +1,12 @@
+const http= require("http");
+const port = 8081;
+
+http.createServer((req,res)=>{
+        res.writeHead(200,{"Content-Type":"text/html"});
+        res.write("<h2> hey server started :-) </h2>");
+        res.end();
+    })
+
+    .listen(port,()=>{
+        console.log(`Node JS server started running on port: ${port}`);
+    });
